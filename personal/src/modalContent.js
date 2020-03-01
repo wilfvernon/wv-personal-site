47653406254517a979364ca16111c683eee4e446
wilf-vernon <wilf.vernon@gmail.com>
2020-03-01 22:12:55 +0000

    some data for modals made

---
+++ personal/src/modalContent.js
@@ -0,0 +1,5 @@
+export const About = {description: "Wilf Vernon is a full-stack software engineer.", skills: ["Javascript", "React", "Ruby", "Rails", "HTML", "CSS", "Redux"]}
+
+export const Projects = [{title: "Company", 
+                        description: "Company is a website designed for players of Final Fantasy XIV, an online video game, to communicate with one another and organise in-game events together. Users are able to connect their Company account to a real FFXIV account and access information about their character and the game and create detailed events with clearly defined player roles."},
+                        ]

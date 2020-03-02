214728c92494b94fb17963fe726c9d3ab4af3294
wilf-vernon <wilf.vernon@gmail.com>
2020-03-02 12:34:43 +0000

    brush animation added

--- personal/src/components/InfoModal.js
+++ personal/src/components/InfoModal.js
@@ -10,12 +10,7 @@ const InfoModal = (props) => {
         <div className={`cd-modal ${props.visible ? 'visible' : ''} ${props.content}`}>
             <div className="modal-content">
                <h1>{props.content}</h1>
-
-               <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                  Ad modi repellendus, optio eveniet eligendi molestiae? 
-                  Fugiat, temporibus! 
-               </p>
+               <img id="wilf-portrait" src="/wilf-watercolor.png" alt="portrait"/>
             <button onClick={closeModal}>Close</button>
             </div> 
         </div>
--- personal/src/components/Watercolor.js
+++ personal/src/components/Watercolor.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import '../ReebokTransition.css'
 
 const Watercolor = () => {
     return (
+        <Fragment>
+        <div className="empty-div"></div>
         <div className="watercolor-div">
             <img src="/sacrecouer-sunga-park.jpg" alt="sacrecouer"/>
         </div>
+        </Fragment>
     );
 }
  
